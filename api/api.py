8934bb50838180da299ed23198b4a6abfd5e7604
WojciechDopieralski <wojciech.dopieralski@op.pl>
2020-08-04 20:06:44 +0200

    added function that picks paramters important for task.

--- api/api.py
+++ api/api.py
@@ -15,16 +15,9 @@ def home():
 
 app.run()
 
-def get_books_json(url: str = "https://www.googleapis.com/books/v1/volumes?q=war") -> dict:
-    
-    user_url = url
-    r = requests.get(user_url)
-    
-    return r.json()
-    
 
 
-#Pobierz json i przetwórz go do odpowiedniej postaci:
+#Pobierz json i przetwórz go do odpowiedniej postaci: 
 # Potrzebe z jsona do zadan sa: id, volumeInfo: authors (cala lista), volumeInfo: publishedDate
 # do wyswitlania potrzebne dodatkowa:  volumeInfo: title, volumeInfo: categories, volumeInfo: averageRating, volumeInfo: raitingsCount, imageLinks: thumbnail
 # Stworz baze SQLite z tymi ksiazkami (startowy data set moze byc pierwszymi 20 elementaki jsona)
--- api/functions.py
+++ api/functions.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 
 def get_books_json(url: str = "https://www.googleapis.com/books/v1/volumes?q=war") -> dict:
@@ -23,3 +24,31 @@ def get_books_json(url: str = "https://www.googleapis.com/books/v1/volumes?q=war
     
     return r.json()
 
+def get_book_information(url: str = "https://www.googleapis.com/books/v1/volumes?q=war"):
+    """
+    TO DO description 
+    
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    None.
+    """
+    
+    url_json = get_books_json(url)
+    items_list = url_json["items"]
+    list_of_wanted_params = ["id", "volumeInfo_authors",  "volumeInfo_title", "volumeInfo_publishedDate", "volumeInfo_categories", "volumeInfo_averageRating", "volumeInfo_ratingsCount", "volumeInfo_imageLinks_thumbnail"]
+    parameters_list = []
+    
+    for item in items_list:
+        flatten_df = pd.json_normalize(item, sep='_')
+        flatten_dict = flatten_df.to_dict(orient='records')[0]  
+        book_params_dict = {key: flatten_dict.get(key) for key in list_of_wanted_params}
+        parameters_list.append(book_params_dict)
+        
+    return parameters_list
+        
+
+
+       
