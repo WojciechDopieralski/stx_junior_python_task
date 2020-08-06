01f939232f072961373e2c90abfd8db921f4252c
WojciechDopieralski <wojciech.dopieralski@op.pl>
2020-08-06 17:35:14 +0200

    Finished search by year and sort

--- api/api.py
+++ api/api.py
@@ -1,5 +1,9 @@
 import flask
-import requests
+from dbconn import db
+import datetime
+from flask_rest_jsonapi import Api
+
+
 
 app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
@@ -11,8 +15,68 @@ def home():
     
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+
+@app.route('/books', methods=['GET'])
+def get_book_list():
+    books_colection = db.books
+    books_bonson = books_colection.find()
+    books_list = []
+    for book in books_bonson:
+        book.pop('_id')
+        books_list.append(book)
+        
+    if 'published_date' in flask.request.args:
+        year = int(flask.request.args['published_date'])
+        year_filter_book_list = []
+        for book in books_list:
+            book_date = book['publishedDate']
+            if "-" in book_date:
+                date_time_obj = datetime.datetime.strptime(book_date, '%Y-%m-%d')
+                book_re_year = int(date_time_obj.year)
+            else:
+                book_re_year = int(book['publishedDate'])
+            
+            if  book_re_year == year:
+                year_filter_book_list.append(book)
+        return flask.jsonify(year_filter_book_list)
+    
+    if 'sort' in flask.request.args:
+        sort_value = str(flask.request.args['sort'])
+        correct_date_list = []
+        for book in books_list:
+            book_date = book['publishedDate']
+            if "-" in book_date:
+                date_time_obj = datetime.datetime.strptime(book_date, '%Y-%m-%d')
+                book_re_year = int(date_time_obj.year)
+            else:
+                book_re_year = int(book['publishedDate'])
+            book['publishedDate'] = book_re_year
+            correct_date_list.append(book)
+            
+        if sort_value == "published_date":
+            sorted_list = sorted(correct_date_list, key = lambda i: i['publishedDate'])
+        elif sort_value == "-published_date":
+            sorted_list = sorted(correct_date_list, key = lambda i: i['publishedDate'], reverse=True)
+            
+        return flask.jsonify(sorted_list)
+    
+    if 'author' in flask.request.args:
+        query_parameters = flask.request.args.to_dict(flat=False)
+        author = query_parameters.get('author')
+        #Z tym mozna dalej dzialac by sprawdzic czy dany autor cos pisal ;) 
+        
+        return 
+        
+    return flask.jsonify(books_list)
+
+
+
+
 app.run()
-#Pobierz json i przetwórz go do odpowiedniej postaci: 
-# Potrzebe z jsona do zadan sa: id, volumeInfo: authors (cala lista), volumeInfo: publishedDate
-# do wyswitlania potrzebne dodatkowa:  volumeInfo: title, volumeInfo: categories, volumeInfo: averageRating, volumeInfo: raitingsCount, imageLinks: thumbnail
-# Stworz baze SQLite z tymi ksiazkami (startowy data set moze byc pierwszymi 20 elementaki jsona)
+
+
+api = Api(app)
+
+
+
+
