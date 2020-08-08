51b2acc1ecd52a90e7719658984c15b083b643c5
WojciechDopieralski <wojciech.dopieralski@op.pl>
2020-08-08 15:02:52 +0200

    refactored get_book_list(), now it sorts and filter in DB querry not python code.

--- api/api.py
+++ api/api.py
@@ -1,64 +1,31 @@
 import flask
 from dbconn import db
-import datetime
 from functions import get_book_information
 
 
 app = flask.Flask(__name__)
-#app.config["DEBUG"] = True
-#nie umiem odczytac w tym errora :/ 
-
 
 @app.route('/', methods=['GET'])
 def home():
     
-    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
-
+    return ""
 
 @app.route('/books', methods=['GET'])
 def get_book_list():
     books_colection = db.books
-    books_bonson = books_colection.find()
-    books_list = []
-    for book in books_bonson:
-        book.pop('_id')
-        books_list.append(book)
+    books_bonson_querry = books_colection.find()
         
     if 'published_date' in flask.request.args:
         year = int(flask.request.args['published_date'])
-        year_filter_book_list = []
-        for book in books_list:
-            book_date = book['publishedDate']
-            if "-" in book_date:
-                date_time_obj = datetime.datetime.strptime(book_date, '%Y-%m-%d')
-                book_re_year = int(date_time_obj.year)
-            else:
-                book_re_year = int(book['publishedDate'])
-            
-            if  book_re_year == year:
-                year_filter_book_list.append(book)
-        return flask.jsonify(year_filter_book_list)
+        books_bonson_querry = books_colection.find({"publishedYear" : {"$eq": year}})
     
     if 'sort' in flask.request.args:
         sort_value = str(flask.request.args['sort'])
-        correct_date_list = []
-        for book in books_list:
-            book_date = book['publishedDate']
-            if "-" in book_date:
-                date_time_obj = datetime.datetime.strptime(book_date, '%Y-%m-%d')
-                book_re_year = int(date_time_obj.year)
-            else:
-                book_re_year = int(book['publishedDate'])
-            book['publishedDate'] = book_re_year
-            correct_date_list.append(book)
-            
         if sort_value == "published_date":
-            sorted_list = sorted(correct_date_list, key = lambda i: i['publishedDate'])
+            books_bonson_querry = books_colection.find().sort("publishedYear", 1)
         elif sort_value == "-published_date":
-            sorted_list = sorted(correct_date_list, key = lambda i: i['publishedDate'], reverse=True)
-            
-        return flask.jsonify(sorted_list)
-    
+            books_bonson_querry = books_colection.find().sort("publishedYear",-1)
+  
     if 'author' in flask.request.args:
         query_parameters = flask.request.args.to_dict(flat=False)
         authors = query_parameters.get('author')
@@ -72,9 +39,13 @@ def get_book_list():
                 books_with_auth_filter.append(book)
          
         return flask.jsonify(books_with_auth_filter)
+    
+    books_list = []
+    for book in books_bonson_querry:
+        book.pop('_id')
+        books_list.append(book)
         
     return flask.jsonify(books_list)
-
 
 @app.route('/books/<string:book_id>', methods=['GET'])   
 def show_book_by_id(book_id):
@@ -85,7 +56,6 @@ def show_book_by_id(book_id):
         books_list.append(book)
     
     return flask.jsonify(books_list)
-
 
 @app.route('/db', methods=['POST'])
 def post_db():
