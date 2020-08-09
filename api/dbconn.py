02ec5351fd4125fdcd0e97060b4b0e00f112ef8a
WojciechDopieralski <wojciech.dopieralski@op.pl>
2020-08-09 15:18:16 +0200

    secured DB password in file

--- api/dbconn.py
+++ api/dbconn.py
@@ -1,7 +1,11 @@
 import pymongo
 import urllib 
+import pickle
 
-uri = 'mongodb+srv://test_user:{}@stxtest.z2ugu.mongodb.net/stxtest?retryWrites=true&w=majority'.format(urllib.parse.quote('STx2020Test@$'))
+with open("pass.p", "rb") as f:
+    mongo_db_pass = pickle.load(f)
+
+uri = 'mongodb+srv://test_user:{}@stxtest.z2ugu.mongodb.net/stxtest?retryWrites=true&w=majority'.format(urllib.parse.quote(mongo_db_pass))
 
 client = pymongo.MongoClient(uri)
 
